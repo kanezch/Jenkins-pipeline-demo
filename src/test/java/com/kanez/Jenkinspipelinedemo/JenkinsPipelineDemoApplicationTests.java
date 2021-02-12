4e07791226f4ff36aca8f77e6dc6d698c6ae49bb
Kane Zheng <Kane.Zheng@resmed.com.au>
2021-02-12 22:40:28 +1100

    first commit

---
+++ src/test/java/com/kanez/Jenkinspipelinedemo/JenkinsPipelineDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kanez.Jenkinspipelinedemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JenkinsPipelineDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
